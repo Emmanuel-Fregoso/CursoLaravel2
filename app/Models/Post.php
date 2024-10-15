afb5478173ebfe5371b865a9cc5f88328d429043
Emmanuel-Fregoso <luis.fregoso4195@alumnos.udg.mx>
2024-10-15 11:23:23 -0600

    Add: Models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,4 +9,21 @@ class Post extends Model
 {
     /** @use HasFactory<\Database\Factories\PostFactory> */
     use HasFactory;
+
+    protected $fillable = ['title', 'content', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'taged_post')->withTimestamps();
+    }
+
+    public function likedUsers()
+    {
+        return $this->belongsToMany(User::class, 'liked_post')->withTimestamps();
+    }
 }
